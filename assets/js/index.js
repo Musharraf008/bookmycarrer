b62b8145b741fb0f01a7804d27b4885ab6eb026d
Musharraf008 <121239615+Musharraf008@users.noreply.github.com>
2023-07-04 06:14:23 +0530

    Update url

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,7 @@ $("#update_user").submit(function(event){
 
     // console.log(data);
     let request = {
-        "url" : `http://localhost:3000/api/users/${data.id}`,
+        "url" : `https://bookmycarrer.onrender.com//api/users/${data.id}`,
         "method" : "PUT",
         "data" : data
     }
@@ -30,7 +30,7 @@ if(window.location.pathname == "/"){
         var id = $(this).attr("data-id")
 
         var request = {
-            "url" : `http://localhost:3000/api/users/${id}`,
+            "url" : `https://bookmycarrer.onrender.com/api/users/${id}`,
             "method" : "DELETE"
         }
 
